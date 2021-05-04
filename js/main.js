62bc9c4704ce818722d69e5b1c79e6ffd376f28f
xuyumi <isabelle.xu88@gmail.com>
2021-05-04 16:02:20 -0400

    homepage

--- js/main.js
+++ js/main.js
@@ -2,23 +2,59 @@ window.onload = function () {
     console.log("Window onload");
     addSmoothScrolling();
     fadeInOnScroll();
+    initNavLinks();
+
 }
 
-// Add smooth scrolling to all <a> tags
-function addSmoothScrolling() {
-    $("a").on('click', function (event) {
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-            let hash = this.hash;
+function initNavLinks() {
+    let pages = document.getElementsByClassName("page");
+    let navLinks = document.getElementsByClassName("navLink");
+    for (let i = 0; i < pages.length; ++i ) {
+        console.log("init " + pages[i].id);
+        let targetId = pages[i].id;
+        navLinks[i].onclick = function() {
+            console.log(targetId + " clicked");
+            showPage(targetId);
+        } 
+    }
 
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-                window.location.hash = hash;
-            });
-        }
-    });
+    // init home (Isabelle Xu)
+    document.getElementsByClassName("logo")[0].onclick = function() {
+        console.log("logo clicked");
+        showPage("home");
+    } 
+}
+
+/**
+ * Single page design implementation.
+ * Show page with given id and hide all other pages with a fade transition.
+ * @argument id
+ */
+function showPage(id) {
+    // hide all pages
+    $( ".page" ).hide();
+    // show page with input id
+    $("#" + id).fadeIn();
+}
+
+// Add smooth scrolling to all elements of class "smoothA"
+function addSmoothScrolling() {
+    let smoothIds = document.getElementsByClassName("smoothA");
+    for (let i = 0; i < smoothIds.length; ++i) {
+        $(smoothIds[i]).on('click', function (event) {
+            if (this.hash !== "") {
+                // Prevent default anchor click behavior
+                event.preventDefault();
+                let hash = this.hash;
+    
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function () {
+                    window.location.hash = hash;
+                });
+            }
+        });
+    }
 }
 
 function fadeInOnScroll() {
