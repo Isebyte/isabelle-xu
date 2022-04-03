9cd171eab8cdc788c40f0efd4155dcae450590ca
Ying Zhang <ying@Yings-MacBook-Pro.local>
2022-04-03 01:27:10 -0400

    fix slideshow

--- js/main.js
+++ js/main.js
@@ -1,26 +1,16 @@
 
-let currSlide = 0; // default slide set to 0
-
 /**
  * Attaches eventlisteners to every gallery image to open the corresponding slide in the modal carosel
  */
-function initImgCaroselToggle() {
+function initImgCarouselToggle() {
     let galleryImgs = document.getElementsByClassName("gallery-img");
+    let myCarouselEl = document.getElementById('carousel');
+    let carousel = new bootstrap.Carousel(myCarouselEl);
+    console.log(carousel);
     for (let i = 0; i < galleryImgs.length; i++) {
-        galleryImgs[i].addEventListener("click", function(){setSlide(i)});
+        galleryImgs[i].addEventListener("click", function(){
+            console.log("Showing slide %s", i);
+            carousel.to(i);
+        });
     }
-}
-
-/**
- * Sets the gallery carosel to show slide # i
- * @param {*} i the slide number to show
- */
-function setSlide(i) {
-    console.log("Showing slide #" + i);
-    // remove active class from previous shown slide
-    let prevSlide = document.getElementById(currSlide);
-    prevSlide.classList.remove("active");
-    let nextSlide = document.getElementById(i);
-    nextSlide.classList.add("active");
-    currSlide = i;
 }
